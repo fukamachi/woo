e32d4f10437794f60711f1ff1bdafe83313fe049
Eitaro Fukamachi <e.arrows@gmail.com>
2016-09-13 15:48:58 +0900

    Don't run all handler tests for IPv6 address.

--- t/ipv6.lisp
+++ t/ipv6.lisp
@@ -1,7 +1,24 @@
 (in-package :cl-user)
 (defpackage woo-test.ipv6
-  (:use :cl))
+  (:use :cl
+        :prove)
+  (:import-from :clack.test
+                :subtest-app
+                :*clackup-additional-args*)
+  (:import-from :clack.test.suite
+                :localhost))
 (in-package :woo-test.ipv6)
 
+(plan 1)
+
 (let ((clack.test:*clackup-additional-args* '(:address "::")))
-  (clack.test.suite:run-server-tests :woo))
+  (subtest-app "IPv6"
+      (lambda (env)
+        (declare (ignore env))
+        '(200 (:content-type "text/plain") ("Hello" "World")))
+    (multiple-value-bind (body status)
+        (dex:get (localhost))
+      (is status 200)
+      (is body "HelloWorld"))))
+
+(finalize)
