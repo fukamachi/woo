561aae75d6aec20d43cff7392939f890f5d6862c
Eitaro Fukamachi <e.arrows@gmail.com>
2017-07-20 21:00:10 +0900

    Fix the status line issue.

--- src/response.lisp
+++ src/response.lisp
@@ -20,94 +20,91 @@
 (declaim (inline wev:write-socket-data wev:write-socket-byte))
 
 (defun status-code-to-text (code)
-  (or (cond
-        ((< code 200)
-         (case code
-           (100 "Continue")
-           (101 "Switching Protocols")
-           (102 "Processing")))
-        ((< code 300)
-         (case code
-           (200 "OK")
-           (201 "Created")
-           (202 "Accepted")
-           (203 "Non-Authoritative Information")
-           (204 "No Content")
-           (205 "Reset Content")
-           (206 "Partial Content")
-           (207 "Multi-Status")
-           (208 "Already Reported")
-           (226 "IM Used")))
-        ((< code 400)
-         (case code
-           (300 "Multiple Choices")
-           (301 "Moved Permanently")
-           (302 "Found")
-           (303 "See Other")
-           (304 "Not Modified")
-           (305 "Use Proxy")
-           (307 "Temporary Redirect")
-           (308 "Permanent Redirect")))
-        ((< code 500)
-         (case code
-           (400 "Bad Request")
-           (401 "Unauthorized")
-           (402 "Payment Required")
-           (403 "Forbidden")
-           (404 "Not Found")
-           (405 "Method Not Allowed")
-           (406 "Not Acceptable")
-           (407 "Proxy Authentication Required")
-           (408 "Request Time-out")
-           (409 "Conflict")
-           (410 "Gone")
-           (411 "Length Required")
-           (412 "Precondition Failed")
-           (413 "Request Entity Too Large")
-           (414 "Request-URI Too Large")
-           (415 "Unsupported Media Type")
-           (416 "Requested range not satisfiable")
-           (417 "Expectation Failed")
-           (418 "I'm a teapot")
-           (421 "Misdirected Request")
-           (422 "Unprocessable Entity")
-           (423 "Locked")
-           (424 "Failed Dependency")
-           (426 "Upgrade Required")
-           (451 "Unavailable For Legal Reasons")))
-        (T
-         (case code
-           (500 "Internal Server Error")
-           (501 "Not Implemented")
-           (502 "Bad Gateway")
-           (503 "Service Unavailable")
-           (504 "Gateway Time-out")
-           (505 "HTTP Version not supported")
-           (506 "Variant Also Negotiates")
-           (507 "Insufficient Storage")
-           (508 "Loop Detected")
-           (509 "Bandwidth Limit Exceeded")
-           (510 "Not Extended"))))
-      (progn
-        (warn "Unknown status code: ~A" code)
-        "")))
+  (cond
+    ((< code 200)
+     (case code
+       (100 "Continue")
+       (101 "Switching Protocols")
+       (102 "Processing")))
+    ((< code 300)
+     (case code
+       (200 "OK")
+       (201 "Created")
+       (202 "Accepted")
+       (203 "Non-Authoritative Information")
+       (204 "No Content")
+       (205 "Reset Content")
+       (206 "Partial Content")
+       (207 "Multi-Status")
+       (208 "Already Reported")
+       (226 "IM Used")))
+    ((< code 400)
+     (case code
+       (300 "Multiple Choices")
+       (301 "Moved Permanently")
+       (302 "Found")
+       (303 "See Other")
+       (304 "Not Modified")
+       (305 "Use Proxy")
+       (307 "Temporary Redirect")
+       (308 "Permanent Redirect")))
+    ((< code 500)
+     (case code
+       (400 "Bad Request")
+       (401 "Unauthorized")
+       (402 "Payment Required")
+       (403 "Forbidden")
+       (404 "Not Found")
+       (405 "Method Not Allowed")
+       (406 "Not Acceptable")
+       (407 "Proxy Authentication Required")
+       (408 "Request Time-out")
+       (409 "Conflict")
+       (410 "Gone")
+       (411 "Length Required")
+       (412 "Precondition Failed")
+       (413 "Request Entity Too Large")
+       (414 "Request-URI Too Large")
+       (415 "Unsupported Media Type")
+       (416 "Requested range not satisfiable")
+       (417 "Expectation Failed")
+       (418 "I'm a teapot")
+       (421 "Misdirected Request")
+       (422 "Unprocessable Entity")
+       (423 "Locked")
+       (424 "Failed Dependency")
+       (426 "Upgrade Required")
+       (451 "Unavailable For Legal Reasons")))
+    (T
+     (case code
+       (500 "Internal Server Error")
+       (501 "Not Implemented")
+       (502 "Bad Gateway")
+       (503 "Service Unavailable")
+       (504 "Gateway Time-out")
+       (505 "HTTP Version not supported")
+       (506 "Variant Also Negotiates")
+       (507 "Insufficient Storage")
+       (508 "Loop Detected")
+       (509 "Bandwidth Limit Exceeded")
+       (510 "Not Extended")))))
 
 (defvar *status-line* (make-hash-table :test 'eql))
 
 (defun http/1.1 (code)
-  (format nil "HTTP/1.1 ~A ~A~C~C"
-          code
-          (status-code-to-text code)
-          #\Return
-          #\Newline))
+  (let ((status-text (status-code-to-text code)))
+    (when status-text
+      (format nil "HTTP/1.1 ~A ~A~C~C"
+              code
+              status-text
+              #\Return
+              #\Newline))))
 
-(loop for status in '(100 101
-                      200 201 202 203 204 205 206
-                      300 301 302 303 304 305 307
-                      400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417
-                      500 501 502 503 504 505)
-      do (setf (gethash status *status-line*)
-               (trivial-utf-8:string-to-utf-8-bytes (http/1.1 status))))
+(loop for status from 100 to 510
+      for status-line = (http/1.1 status)
+      when status-line
+        do (setf (gethash status *status-line*)
+                 (trivial-utf-8:string-to-utf-8-bytes status-line)))
 
 (defvar *empty-chunk*
   #.(trivial-utf-8:string-to-utf-8-bytes (format nil "0~C~C~C~C"
