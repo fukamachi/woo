621ab7c4888e8a2d289beaadbeed9aea14709abb
Eitaro Fukamachi <e.arrows@gmail.com>
2024-08-11 14:18:33 +0000

    Run testing also for HTTPS.

--- t/woo.lisp
+++ t/woo.lisp
@@ -6,3 +6,11 @@
 
 (deftest woo-server-tests
   (clack.test.suite:run-server-tests :woo))
+
+(deftest woo-ssl-server-tests
+  (let ((clack.test:*clackup-additional-args*
+          '(:ssl-cert-file #P"t/certs/localhost.crt"
+            :ssl-key-file #P"t/certs/localhost.key"))
+        (dex:*not-verify-ssl* t)
+        (clack.test:*use-https* t))
+    (clack.test.suite:run-server-tests :woo)))
